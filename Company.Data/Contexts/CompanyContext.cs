6ce55b786a5e1864165545ca3c26109bd80bbb74
rubenriseld <rubenriseld@outlook.com>
2022-12-20 10:09:38 +0100

    base projects and packages

---
+++ Company.Data/Contexts/CompanyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Contexts;
+
+public class CompanyContext : DbContext
+{
+	public CompanyContext(DbContextOptions<CompanyContext> options) : base(options)
+	{
+
+	}
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		base.OnModelCreating(builder);
+	}
+}
